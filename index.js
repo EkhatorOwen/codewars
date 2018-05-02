43a3c06a06c0453594d34c426b732ab2a5767890
Owen Ekhator <oe52@gmail.com>
2018-05-02 09:15:20 -0500

    solved fizzbuzz

--- index.js
+++ index.js
@@ -18,3 +18,43 @@ function isAscOrder(arr)
 }
 
 console.log(isAscOrder([1,2,4,7,19]));
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value 'Fizz' instead
+// If the value is a multiple of 5: use the value 'Buzz' instead
+// If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+
+var arr = [];
+function FizzBuzz(n)
+{
+  for(i=1;i<=n;i++)
+  {
+    if(i%3===0&&i%5===0)
+    {
+      //console.log('FizzBuzz');
+      arr.push('FizzBuzz')
+    }
+    else if(i%5===0)
+    {
+     // console.log('Buzz');
+     arr.push('Buzz');
+    }
+   else if(i%3===3)
+    {
+     // console.log('Fizz');
+     arr.push('Fizz')
+    }
+    else
+    {
+       //console.log(i);
+       arr.push(i);
+    }
+     
+  }
+  return arr;
+}
+
+console.log(FizzBuzz(20));

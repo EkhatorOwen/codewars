db0300f09c9ba95b96b62317c246da2e7abecca0
Owen Ekhator <oe52@gmail.com>
2018-05-24 10:37:22 -0500

    completed

--- index.js
+++ index.js
@@ -301,3 +301,35 @@ function fibo(n){
 
   arr.push[b]
 }
+
+// Description:
+
+// Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you do, but exactly how many? Maybe it's time to write a program to figure it out.
+
+// You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+// (If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+
+// Note: Of course you can't do half a fold. You should know what this means ;P
+
+// Also, if somebody is giving you a non-positive distance, it's clearly bogus and you should yell at them by returning null/NULL.
+
+//384,400 km to the moon
+
+//https://www.codewars.com/kata/folding-your-way-to-the-moon
+
+
+function folds( dist ) {
+  if ( dist < 0 ) return null;
+  var distMeters = dist * 1000;
+  var distanceCovered = 0.0001;
+  var foldCount = 0;
+  while ( distanceCovered <= distMeters ) {
+    foldCount++;
+    distanceCovered += distanceCovered;
+   // console.log( 'distanceCovered: ', distanceCovered );
+  }
+  console.log( 'distMeters: ', distMeters );
+  console.log( 'foldCount: ', foldCount );
+  return foldCount;
+}
+folds( 384400 );

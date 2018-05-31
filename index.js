1a1be2c443467aeb7d9ea2b55dff687310b01f74
Owen Ekhator <oe52@gmail.com>
2018-05-31 09:33:13 -0500

    completed

--- index.js
+++ index.js
@@ -509,3 +509,19 @@ function validParenthesesTest(parant){
 //  return count ==count2
 // }
 
+// Create a function that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+
+// Examples:
+
+// findNextSquare(121) --> returns 144
+// findNextSquare(625) --> returns 676
+// findNextSquare(114) --> returns -1 since 114 is not a perfect
+
+  function findNextSquare(n){
+  return  Number.isInteger( Math.sqrt(n))?Math.pow( Math.sqrt(n)+1,2): -1
+  
+  }
+  
+  console.log(findNextSquare(121))

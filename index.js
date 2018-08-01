cd30b11dfdb12dd58704aaa130ea59fcd94d3c08
Owen Ekhator <owenekhator@rocketmail.com>
2018-07-31 22:20:18 -0500

    completed

--- index.js
+++ index.js
@@ -580,3 +580,21 @@ function uniq(arr){
   
   }
   }
+
+//   You have an array of numbers.
+// Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+// Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+function sortArray(array) {
+  let odds = array.filter(element=>element%2!==0).sort((a,b)=>a>b);  
+  console.log(odds)
+  return array.map(element=>{
+    if(element%2!==0){
+        return odds.shift()
+    }else
+    {
+      return element
+    }
+  })
+}

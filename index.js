8fdcb9bc646dfc0d6a57a186cec0fad09f7a73bc
Owen Ekhator <owenekhator@rocketmail.com>
2018-09-18 10:44:38 -0500

    completed new challenge

--- index.js
+++ index.js
@@ -828,3 +828,22 @@ Given a string str, reverse it omitting all non-alphabetic characters.
 function reverseLetter(str){
   return  str.match(/[a-zA-Z]/gm).reverse().join('')
   }
+
+  //Timmy & Sarah think they are in love,
+  // but around where they live, they will only know once they pick a flower each. 
+  //If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+//Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  // moment of truth
+ // console.log(flower1%2)
+  if(flower1%2===0 && flower2%2===0){  
+      return false
+  } else if (flower1%2==!0 && flower2%2==!0){
+      
+      return false
+  }
+  
+  return true
+ 
+}

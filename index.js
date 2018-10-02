cf4596bca3186410d6bfcd8865cb13ce5f034988
Owen Ekhator <owenekhator@rocketmail.com>
2018-10-02 00:26:25 -0500

    completed

--- index.js
+++ index.js
@@ -864,3 +864,59 @@ function timedReading(maxLength, text) {
   })
   return count
 }
+
+/*Write a function done_or_not/DoneOrNot passing a board (list[list_lines]) as parameter. 
+If the board is valid return 'Finished!', otherwise return 'Try again!' 
+Test.assertEquals(doneOrNot([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]]), "Finished!");
+*/
+function doneOrNot(board){
+
+  for(let i=0; i<board.length; i++){
+     if(!checkDuplicates(board[i])){
+       return 'Try again!';
+     }
+    }
+    let newArr =[];
+    let arrz = [];
+    for(let i=0; i<board.length;i++){ 
+      for(let j=0; j<board[i].length;j++){
+        newArr.push(board[j][i])
+      }
+      if(!checkDuplicates(newArr)){
+        return 'Try again!';
+      }   
+     newArr =[];
+    }
+     for(let i=0; i<3;i++){
+      for(let j =0; j<3; j++){
+    
+        newArr.push(board[j][i]) 
+      }
+      arrz = [...arrz, ...newArr]
+      newArr =[];
+    }
+    if(!checkDuplicates(arrz)){
+      return 'Try again!';
+    }
+    
+    return 'Finished!'
+}
+
+function checkDuplicates(arr){
+  let newArr = [];
+
+  for(let i=0;i<arr.length;i++){
+    if(!newArr.includes(arr[i])){
+      newArr.push(arr[i])
+    }
+  }
+  return arr.length === newArr.length
+}

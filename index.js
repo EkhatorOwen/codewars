9070254f6bc0aa3d3de95b0b20953721403465a8
Owen Ekhator <owenekhator@rocketmail.com>
2018-10-11 19:27:31 -0500

    completed

--- index.js
+++ index.js
@@ -972,3 +972,14 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
+//  Return the truncated string with a ... ending.
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  //console.log(str)
+  if(num<str.length){
+  return str.slice(0,num).concat('...');
+  }
+ return str
+}

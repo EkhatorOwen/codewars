57a3fd105d99853e827a7baba32aa26d0f412769
Owen Ekhator <owenekhator@rocketmail.com>
2018-10-14 23:27:07 -0500

    completed

--- index.js
+++ index.js
@@ -994,3 +994,14 @@ function findElement(arr, func) {
   }
   return undefined
 }
+
+//Check if a value is classified as a boolean primitive. Return true or false.
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+if(typeof bool==="boolean"){
+  return true
+}
+  return false;
+}
+
+booWho(true);

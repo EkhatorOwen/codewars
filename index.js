e30ff7867f391a36ba58fb06b1fb40407a79d0fc
Owen Ekhator <owenekhator@rocketmail.com>
2018-11-01 22:16:41 -0500

    completed

--- index.js
+++ index.js
@@ -1097,3 +1097,20 @@ function sumAll(arr) {
   }
   return sum;
 }
+
+//Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  arr1.forEach(element => {
+    if(!arr2.includes(element)){
+      newArr.push(element)
+    }
+  })
+  arr2.forEach(element =>{
+      if(!arr1.includes(element)){
+        newArr.push(element)
+      }
+  })
+  return newArr;
+}

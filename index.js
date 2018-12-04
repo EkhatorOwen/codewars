c7eec0cc05b8559dc1c2576d5f2bff9224a32677
Owen Ekhator <owenekhator@rocketmail.com>
2018-12-03 20:40:25 -0600

    removed comments

--- index.js
+++ index.js
@@ -1405,9 +1405,9 @@ sumFibs(4);
 
 
 function sumPrimes(num) {
-  // step 1	
+  
   let arr = Array.from({length: num+1}, (v, k) => k).slice(2); 
-  // step 2
+  
   let onlyPrimes = arr.filter( (n) => { 
     let m = n-1;
     while (m > 1 && m >= Math.sqrt(n)) { 
@@ -1417,6 +1417,6 @@ function sumPrimes(num) {
     }
       return true;
   });
-  // step 3
+  
   return onlyPrimes.reduce((a,b) => a+b); 
 }

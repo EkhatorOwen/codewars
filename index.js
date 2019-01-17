75d7b44a818047d3454d4dde20f8cac25b4d4051
Owen Ekhator <owenekhator@rocketmail.com>
2019-01-16 23:33:51 -0600

    added new

--- index.js
+++ index.js
@@ -1587,3 +1587,28 @@ var Person = function(firstAndLast) {
   }
 
 }
+
+//Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var a = 2 * Math.PI;
+  var newArr = [];
+  var getOrbPeriod = function(obj) {
+    var c = Math.pow(earthRadius + obj.avgAlt, 3);
+    var b = Math.sqrt(c / GM);
+    var orbPeriod = Math.round(a * b);
+    delete obj.avgAlt;
+    obj.orbitalPeriod = orbPeriod;
+    return obj;
+  };
+
+  for (var elem in arr) {
+    newArr.push(getOrbPeriod(arr[elem]));
+  }
+
+  return newArr;
+}
+
+// test here
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);

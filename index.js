a26664f721b8e06803e5463ab77183a3721619c7
Owen Ekhator <owenekhator@rocketmail.com>
2019-07-25 23:16:22 -0500

    added new problem

--- index.js
+++ index.js
@@ -1852,3 +1852,10 @@ for (let i=0; i<n; i++){
 }
 
 return num
+
+/*
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
